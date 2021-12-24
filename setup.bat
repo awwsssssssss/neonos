c3b71e2a90d5183b35d5a942a1bf2452fc0dec37
awwsssssssss <85400732+awwsssssssss@users.noreply.github.com>
2021-12-24 20:02:58 +0700

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,3 +1,6 @@
+@echo off
+echo any key for start install
+pause
 if NOT EXIST slot.xcdn https://raw.githubusercontent.com/awwsssssssss/neonos/main/data/meta/slot.xcdn
 if NOT EXIST uni.h curl https://raw.githubusercontent.com/awwsssssssss/neonos/main/uni.h >uni.h
 if NOT EXIST neonos.cmd curl https://raw.githubusercontent.com/awwsssssssss/neonos/main/neonos.cmd >neonos.cmd
