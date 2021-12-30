dfe02dc88e9406cca87ab0cf05a6c6178b0d1dc6
awwsssssssss <85400732+awwsssssssss@users.noreply.github.com>
2021-12-30 20:31:34 +0700

    Update index.php

--- webserver/index.php
+++ webserver/index.php
@@ -1,6 +1,11 @@
 <?php
 $text = 1;
 ?>
-<img src="https://raw.githubusercontent.com/awwsssssssss/neonos/main/webserver/neonone.png"><h1>Neon <?php echo $text;?>  CFA(CLIENT FLASH AUTOMATIC) Page</h1>
-<p>ITS WORKS</p><br>
-<p>edit index.php</p>
+<div style="color:white;background:red;"><img src="https://raw.githubusercontent.com/awwsssssssss/neonos/main/webserver/neonone.png">NEON ONE PHP WEB SERVER</div><h1>Neon <?php echo $text;?>  CFA(CLIENT FLASH AUTOMATIC) Page</h1>
+<p>ITS WORKS</p>
+<p>EDIT index.php</p>
+<p>Database 2.0 #40</p>
+<p><code>Cpanel Base Config Php Apache</code></p>
+<ab><code>Php 7.0 Neon Client Flash Automatic WinSERVER 2008</code></ab>
+<stdy>Herf Code id::GGR</stdy>
+<ttl>::::1</ttl>
