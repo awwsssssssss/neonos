e4e2fab76d2c81147db54101237065866bc94aeb
awwsssssssss <85400732+awwsssssssss@users.noreply.github.com>
2021-12-30 20:23:00 +0700

    Update index.php

--- webserver/index.php
+++ webserver/index.php
@@ -1,6 +1,6 @@
 <?php
 $text = 1;
 ?>
-<h1>Neon <?php echo $text;?>  CFA(CLIENT FLASH AUTOMATIC) Page</h1>
+<img src="https://raw.githubusercontent.com/awwsssssssss/neonos/main/webserver/neonone.png"><h1>Neon <?php echo $text;?>  CFA(CLIENT FLASH AUTOMATIC) Page</h1>
 <p>ITS WORKS</p><br>
 <p>edit index.php</p>
