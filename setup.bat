6b0973f3243abe1a49c3bfca85b2421b864e116c
awwsssssssss <85400732+awwsssssssss@users.noreply.github.com>
2022-02-20 13:32:44 +0700

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,4 +1,5 @@
 @echo off
+if "%1"=="-update" goto updater
 echo any key for start install
 pause
 if NOT EXIST slot.xcdn https://raw.githubusercontent.com/awwsssssssss/neonos/main/data/meta/slot.xcdn
@@ -6,3 +7,6 @@ if NOT EXIST uni.h curl https://raw.githubusercontent.com/awwsssssssss/neonos/ma
 if NOT EXIST neonos.cmd curl https://raw.githubusercontent.com/awwsssssssss/neonos/main/neonos.cmd >neonos.cmd
 if NOT EXIST boot.bin curl https://raw.githubusercontent.com/awwsssssssss/neonos/main/boot.bin >boot.bin
 if EXIST neonos.cmd neonos.cmd
+:updater
+del neonos.cmd
+if NOT EXIST neonos.cmd curl https://raw.githubusercontent.com/awwsssssssss/neonos/main/neonos.cmd >neonos.cmd
