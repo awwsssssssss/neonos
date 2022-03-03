8f8905fdcd4202f3e72c92c1adaccd078c9ba6d9
awwsssssssss <85400732+awwsssssssss@users.noreply.github.com>
2022-03-03 20:32:07 +0700

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -2,14 +2,16 @@
 if "%1"=="-update" goto updater
 echo any key for start install
 pause
-if NOT EXIST slot.xcdn https://raw.githubusercontent.com/awwsssssssss/neonos/main/data/meta/slot.xcdn
+if NOT EXIST slot.xcdn curl https://raw.githubusercontent.com/awwsssssssss/neonos/main/data/meta/slot.xcdn >slot.xcdn
 if NOT EXIST uni.h curl https://raw.githubusercontent.com/awwsssssssss/neonos/main/uni.h >uni.h
 if NOT EXIST neonos.cmd curl https://raw.githubusercontent.com/awwsssssssss/neonos/main/neonos.cmd >neonos.cmd
 if NOT EXIST boot.bin curl https://raw.githubusercontent.com/awwsssssssss/neonos/main/boot.bin >boot.bin
 if EXIST neonos.cmd neonos.cmd
 :updater
-del neonos.cmd
-if NOT EXIST neonos.cmd curl https://raw.githubusercontent.com/awwsssssssss/neonos/main/neonos.cmd >neonos.cmd
+curl https://raw.githubusercontent.com/awwsssssssss/neonos/main/data/meta/slot.xcdn >slot.xcdn
+curl https://raw.githubusercontent.com/awwsssssssss/neonos/main/uni.h >uni.h
+curl https://raw.githubusercontent.com/awwsssssssss/neonos/main/boot.bin >boot.bin
+curl https://raw.githubusercontent.com/awwsssssssss/neonos/main/neonos.cmd >neonos.cmd
 cls
 color 16
 echo ------------------------------------Neon--Os-------------------------------------------------
